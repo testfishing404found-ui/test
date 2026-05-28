a10b1707cc9e88f2b4ce7ab00caa0058a75d094e
testfishing404found-ui <testfishing404found@gmail.com>
2026-05-28 10:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-os.get_env("BOT_TOKEN")
-os.get_env("CONTROL_CHANNEL_ID")
+os.getenv("BOT_TOKEN")
+os.getenv("CONTROL_CHANNEL_ID")
 
 # ============================================
 # CONFIGURATION - REPLACE WITH YOUR VALUES
